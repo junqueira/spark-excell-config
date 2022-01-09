596d1818d722ddb333cc3e9918ea5ccd046ae95d
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2022-01-09 15:52:50 -0300

    add commit

--- src/main/scala/anp/bussiness/transform/FuelTransform.scala
+++ src/main/scala/anp/bussiness/transform/FuelTransform.scala
@@ -10,7 +10,6 @@ import org.apache.spark.sql.types._
 object FuelTransform extends CommonTransform {
 
     val fuelSchema = StructType(Array(
-<<<<<<< HEAD
         StructField("COMBUSTÍVEL", StringType, nullable = true),
         StructField("ANO", IntegerType, nullable = true),
         StructField("REGIÃO", StringType, nullable = true),
@@ -28,6 +27,26 @@ object FuelTransform extends CommonTransform {
         StructField("Nov", DoubleType, nullable = true),
         StructField("Dez", DoubleType, nullable = true),
         StructField("Total", DoubleType, nullable = true)))
+    
+
+    // val fuelSchema = StructType(Array(
+    //     StructField("COMBUSTÍVEL", StringType, nullable = false),
+    //     StructField("ANO", IntegerType, nullable = false),
+    //     StructField("REGIÃO", StringType, nullable = false),
+    //     StructField("ESTADO", StringType, nullable = false),
+    //     StructField("Jan", DoubleType, nullable = false),
+    //     StructField("Fev", DoubleType, nullable = false),
+    //     StructField("Mar", DoubleType, nullable = false),
+    //     StructField("Abr", DoubleType, nullable = false),
+    //     StructField("Maio", DoubleType, nullable = false),
+    //     StructField("Jun", DoubleType, nullable = false),
+    //     StructField("Jul", DoubleType, nullable = false),
+    //     StructField("Ago", DoubleType, nullable = false),
+    //     StructField("Set", DoubleType, nullable = false),
+    //     StructField("Out", DoubleType, nullable = false),
+    //     StructField("Nov", DoubleType, nullable = false),
+    //     StructField("Dez", DoubleType, nullable = false),
+    //     StructField("Total", DoubleType, nullable = false)))
 
     def execute(spark: SparkSession): DataFrame = {
         log.info(s"Initial read xls fuel... -> ")
@@ -37,43 +56,7 @@ object FuelTransform extends CommonTransform {
         println(s"worksheet: $worksheet quant-> " + ws.count)
         aggRegion(ws, "SÃO PAULO", "2017").show
         ws
-=======
-        StructField("COMBUSTÍVEL", StringType, nullable = false),
-        StructField("ANO", IntegerType, nullable = false)))
-
-    val fuelSchema = StructType(Array(
-        StructField("COMBUSTÍVEL", StringType, nullable = false),
-        StructField("ANO", IntegerType, nullable = false),
-        StructField("REGIÃO", StringType, nullable = false),
-        StructField("ESTADO", StringType, nullable = false),
-        StructField("Jan", DoubleType, nullable = false),
-        StructField("Fev", DoubleType, nullable = false),
-        StructField("Mar", DoubleType, nullable = false),
-        StructField("Abr", DoubleType, nullable = false),
-        StructField("Maio", DoubleType, nullable = false),
-        StructField("Jun", DoubleType, nullable = false),
-        StructField("Jul", DoubleType, nullable = false),
-        StructField("Ago", DoubleType, nullable = false),
-        StructField("Set", DoubleType, nullable = false),
-        StructField("Out", DoubleType, nullable = false),
-        StructField("Nov", DoubleType, nullable = false),
-        StructField("Dez", DoubleType, nullable = false),
-        StructField("Total", DoubleType, nullable = false)))
-
-    def execute(spark: SparkSession): Unit = {
-        try {
-            log.info(s"Initial read xls fuel... -> ")
-            val worksheet = "fuel"
-            val sheetPos = "!A1:Q5000"
-            mergeWorkSheets(spark, worksheet, sheetPos, fuelSchema)
-        } catch {
-            case e: Exception => log.info(e.printStackTrace())
-        }
->>>>>>> add commit
     }
-
-  
-
 
 }
 
